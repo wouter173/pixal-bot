006dd8fa2a7f2175e5acc8538ca22e31ae98a5f3
wouter <wouter173@outlook.com>
2019-09-12 22:43:50 +0200

    help command

---
+++ commands/help.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+const commands = require("../commands.js").getCommands()
+const config = require("../config.json")
+
+module.exports.run = (msg, args) => {
+  const embed = new Discord.RichEmbed()
+    .setTitle("test")
+
+  commands.forEach(command => {
+    let data = require(`./${command}.js`).data
+    embed.addField(data.usage.toUpperCase(), `${data.description}`, true)
+  });
+
+  msg.channel.send(embed)
+}
+
+module.exports.data = {
+  description: "Help command",
+  usage: `${config.prefix}help`
+}
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const Discord = require("discord.js");
+const commands = require("./commands.js").getCommands()
+const config = require("./config.json")
 const Client = new Discord.Client();
 
 Client.on("ready", () => {
-  console.log("ready")
+  console.log("ready.")
 })
 
-Client.on("disconnect", () => {
-  console.log("disconnected.")
+Client.on("message", (msg) => {
+  const args = msg.content.toLowerCase().split(" ")
+  const cmdRaw = args.shift()
+
+  if (!cmdRaw.startsWith(config.prefix)) return
+  if (msg.author.bot) return;
+
+  const cmd = cmdRaw.split(config.prefix)[1]
+
+  for (let i = 0; i < commands.length; i++) {
+    if (cmd == commands[i]) require(`./commands/${commands[i]}.js`).run(msg, args)
+  }
 })
 
 Client.login(process.env.TOKEN)
