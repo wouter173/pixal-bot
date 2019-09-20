ea7843899e0c10f213c4664ab1be637a18ae80f1
wouter <wouter173@outlook.com>
2019-09-20 20:35:21 +0200

    better 8ball and color.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,15 +1,17 @@
 const config = require("../config.json");
 const { generator, error } = require("../utils/embed.js");
-const { RoleColor } = require("../utils/color.js");
+const { roleColor } = require("../utils/color.js");
 
 module.exports.run = (msg, args) => {
-  if (!args.shift()) return msg.channel.send(error("No question specified.", msg.author));
+  if (!args.shift()) {
+    return msg.channel.send(error("No question specified.", msg.author));
+  }
   const question = args.join(" ");
   const desc = `${msg.member.displayName} seeks an answer for: ${question}\n`;
-  msg.channel.send(generator(RoleColor(msg), "8ball", desc, msg.author));
-}
+  msg.channel.send(generator(roleColor(msg), "8ball", desc, msg.author));
+};
 
 module.exports.data = {
   description: "Shake the 8ball.",
   usage: `${config.prefix}8ball <question>`
-}
+};
