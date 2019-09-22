4e1e4bc2e37324fb353b68e97a0017152a62b7af
wouter <wouter173@outlook.com>
2019-09-22 08:43:31 +0200

    8ball done

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,11 +3,15 @@ const { generator, error } = require("../utils/embed.js");
 const { roleColor } = require("../utils/color.js");
 
 module.exports.run = (msg, args) => {
-	if (!args.shift()) {
+	if (!args[0]) {
 		return msg.channel.send(error("No question specified.", msg.author));
 	}
-	const question = args.join(" ");
-	const desc = `${msg.member.displayName} seeks an answer for: ${question}\n`;
+	const possibilities = config.possibilities;
+	let question = args.join(" ");
+	if (question[question.length - 1] !== "?") { question += "?"; }
+
+	const answer = possibilities[Math.floor(Math.random() * possibilities.length)];
+	const desc = `**${msg.member.displayName}** asked: **${question}**\n\n:8ball: **${answer}**`;
 	msg.channel.send(generator(roleColor(msg), "8ball", desc, msg.author));
 };
 
